855669563a659b9b3d903136ccbf362f7ce71f5f
LAB12_USER ON WS16 <L12Y19W16@LabHCI.net>
2025-09-25 19:39:06 +0800

    Add initial SecurityConfig class for application security configuration

---
+++ lapure-jessie/campusequipmentloan/src/main/java/edu/cit/lapure/jessie/campusequipmentloan/controller/HomeController.java
@@ -0,0 +1,18 @@
+package edu.cit.lapure.jessie.campusequipmentloan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui.html";
+    }
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
